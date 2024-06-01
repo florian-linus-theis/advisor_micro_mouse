5fe7bcd1b34051ad3418eeaece4eefd3d7211df8
florian-linus-theis <137921936+florian-linus-theis@users.noreply.github.com>
2024-06-01 22:19:01 +0200

    added systick functions and added first driving tests

--- src/robin.h
+++ src/robin.h
@@ -5,38 +5,11 @@
 #include "stm32f4xx_hal.h"
 #include "Setup\Setup.h"
 #include <display.h>
-#include "Sensors.h"
 
 extern bool encoderTurned;
 
-// bool Error_Flag;
-// int Flag_Mid;  
-// int interrupt_counter;
-// int Messung_Mid;
-// uint32_t Distance_Sensor_Mid_MM;
-
-// int Channel_Emitter[] = {IR_EMITTER_LS, IR_EMITTER_LD, IR_EMITTER_LF, IR_EMITTER_RF, IR_EMITTER_RD, IR_EMITTER_RS};
-// int Channel_Sensoren[] = {IR_SENSOR_LS, IR_SENSOR_LD, IR_SENSOR_LF, IR_SENSOR_RF, IR_SENSOR_RD, IR_SENSOR_RS};
-
-// std::vector<uint32_t> Distanz_Sensoren;
-
-// HardwareTimer timer3(TIM3);
-// HardwareTimer timer4(TIM4);
-HardwareTimer timer8(TIM8);
-HardwareTimer timer9(TIM9);
-
-HardwareSerial Serial1(BLUETOOTH_RX, BLUETOOTH_TX);
-
-void Distanz_Messung_Hell(void);
-void Sensor_Interrupt(void);
-void Sensor_Sync_Setup(void);
-
-void Mid_Sensor_Interrupt(void);
-void Mid_Sensor_Setup(void);
-void Distanz_Mid_Sensor(void);
-
-// void Timer3_Setup(void);
-// void Timer4_Setup(void);
+// HardwareTimer timer8(TIM8);
+// HardwareTimer timer9(TIM9);
 
 void BackwardLeft(int dutyCycle) {
     timer3->setCount(0);
