36ffa18ed93a9228685a89a3f258d2f2f7f813ff
Hannes <hannes.bauer24@gmail.com>
2024-06-04 10:47:52 +0200

    Sensoren: 2. Messung herausgenommen. Puffer auf +100. BT gibt -100 heraus - fuer den User

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -3,7 +3,7 @@
 int Channel_Emitter[] = {IR_EMITTER_LS, IR_EMITTER_LD, IR_EMITTER_LF, IR_EMITTER_RF, IR_EMITTER_RD, IR_EMITTER_RS, IR_EMITTER_MID};
 int Channel_Sensoren[] = {IR_SENSOR_LS, IR_SENSOR_LD, IR_SENSOR_LF, IR_SENSOR_RF, IR_SENSOR_RD, IR_SENSOR_RS, IR_SENSOR_MID};
 int Distance_Sensor[7] = {};
-int calibration_sensor[7] = {137,221,168,175,241,182,337};    //Kalibierung fehlt
+int calibration_sensor[7] = {0,0,0,0,0,0,0};    //Kalibierung fehlt
 bool Walls_Flag[7] ={};
 
 //Meassurement Data Vector
@@ -19,8 +19,7 @@ void Distanz_Messung_Sensoren(void);
 
 void Distanz_Messung_Blind(void){
   for(int i = 0; i < 7; i++){
-    Messuring1 = analogRead(Channel_Sensoren[i]);           //second Messureing should increase accuracy of sensorreadings
-    Distance_Sensor[i] = (Messuring1 + analogRead(Channel_Sensoren[i])) % 2;
+    Distance_Sensor[i] = analogRead(Channel_Sensoren[i]);
   }
 }
 
@@ -29,7 +28,7 @@ void Distanz_Messung_Sensoren(void){
   Distanz_Messung_Hell();
 
   for(int i = 0; i < 7; i++){
-    if (Distance_Sensor[i] > 20){     //Avoids unprecies informations of walls in lay
+    if (Distance_Sensor[i] > 120){     //Avoids unprecies informations of walls in lay
       Walls_Flag[i] = true;       //Platzhalter für Linearisierte Sensorwerte und Auswertung in MM
     }
     else{
@@ -55,10 +54,9 @@ void Distanz_Messung_Hell(void) {
 }
 
 void Timer6_Interrupt(void) { 
-  Distance_Sensor[interrupt_counter] =  analogRead(Channel_Sensoren[interrupt_counter]) - Distance_Sensor[interrupt_counter] - calibration_sensor[interrupt_counter]; // Read Sensor Values
-  if(Distance_Sensor[interrupt_counter] < 0){
-    Distance_Sensor[interrupt_counter] = 0;         //Allow only positive Values
-  }
+  Distance_Sensor[interrupt_counter] =  100 + analogRead(Channel_Sensoren[interrupt_counter]) - Distance_Sensor[interrupt_counter] - calibration_sensor[interrupt_counter]; // Read Sensor Values
+  //10 is new level of calibrated sensors. 100 +. Negative Values are avoided
+
   digitalWrite(Channel_Emitter[interrupt_counter], LOW);                        // Turn off the current emitter
 
   interrupt_counter++;  // Move to the next sensor
@@ -76,6 +74,6 @@ void printDistanzSensoren(void) {
   for (int i = 0; i < 7; i++) {
     ble->print(i);
     ble->print(": ");
-    ble->println(Distance_Sensor[i]);
+    ble->println(Distance_Sensor[i] - 100);   //Userfriendly read
   }
 }
