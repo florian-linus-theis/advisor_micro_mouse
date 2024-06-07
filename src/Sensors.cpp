9effaf632881ba2f69987b8c2cd185a2f49bb4b4
robin <robin.wintrich@gmx.de>
2024-06-07 16:17:02 +0200

    Dynamic Sensor Calibration, ready for Test. Ballgrabber still manual

--- src/Sensors.cpp
+++ src/Sensors.cpp
@@ -149,3 +149,161 @@ void calibrate_sensors(int measurements_air, int measurements_maze){
     }
 
 }
+
+int air_values[7] = {0};
+int maze_values[7] = {0};
+int ballgrabber_values[7] = {0};
+
+void calibration_average(int measurements_air, int measurements_maze);
+
+void calibrate_sensors_dynamic(int measurements_air, int measurements_maze){
+  digitalWrite(LED_GREEN, LOW);
+  Buzzer_beep(3000, 3, 100);
+  SENSORS_CALIBRATED = false; // we start from scratch again
+  
+    
+  // Light LED and Wait for user finger
+  display_print("Now in start square...", 1);
+  digitalWrite(LED_GREEN, HIGH);
+  start();
+
+  Distanz_Messung_Sensoren();
+  air_values[2] += Distance_Sensor[2];    //Left front
+  air_values[5] += Distance_Sensor[5];    //Right front
+  air_values[6] += Distance_Sensor[6];    //Mid front
+  
+
+  move_forward_middle_level(150, 0.45);   //move to the middle of cell
+  rotate_left();
+
+  //2 Messurements for Walls to average PID Errors in Move forward
+
+  for (int i = 0; i<measurements_maze; i++){                        //Definition von Drehpunkt Kurve
+    Distanz_Messung_Sensoren();
+    maze_values[2] += Distance_Sensor[2];    //Left front
+    maze_values[5] += Distance_Sensor[5];    //Right front
+    maze_values[6] += Distance_Sensor[6];    //Mid front
+  }
+
+  turn_around_right();
+
+  for (int i = 0; i<measurements_maze; i++){                        //Definition von Drehpunkt Kurve
+    Distanz_Messung_Sensoren();
+    maze_values[2] += Distance_Sensor[2];    //Left front
+    maze_values[5] += Distance_Sensor[5];    //Right front
+    maze_values[6] += Distance_Sensor[6];    //Mid front
+  }
+
+  Buzzer_beep(3000, 3, 100);
+
+  BackwardLeft(150);
+  BackwardRight(150);
+  delay(500);                             //moves backward against wall
+  stop();
+
+    // measure in the maze and add up to array
+  for (int i=0; i<measurements_maze; i++){
+    Distanz_Messung_Sensoren();
+    for(int j=3; j<5; j++) {                  //only right sensors will see wall
+      maze_values[j] += Distance_Sensor[j];
+      }
+  }
+  //air values left side
+  for (int i=0; i<measurements_air; i++){
+    Distanz_Messung_Sensoren();
+    for(int j=0; j<2; j++) {                  //only left sensors will see air
+      air_values[j] += Distance_Sensor[j];
+    }
+  }
+
+  Buzzer_beep(3000, 3, 100);
+
+  move_actual(150);
+  delay(200);
+  stop();
+  turn_around_right();
+
+  BackwardLeft(150);
+  BackwardRight(150);
+  delay(500);                           //moves againt opposit wall with back
+  stop();
+
+    // measure in the maze and add up to array
+  for (int i=0; i<measurements_maze; i++){
+    Distanz_Messung_Sensoren();
+    for(int j=0; j<2; j++) {                  //only left sensors will see air
+      air_values[j] += Distance_Sensor[j];
+      }
+  }
+  //air values left side
+  for (int i=0; i<measurements_air; i++){
+    Distanz_Messung_Sensoren();
+    for(int j=3; j<5; j++) {                  //only left sensors will see wall
+      maze_values[j] += Distance_Sensor[j];
+    }
+  }
+
+  Buzzer_beep(3000, 3, 100);
+
+  move_actual(150);
+  delay(200);
+  stop();
+  turn_around_right();
+
+  BackwardLeft(150);
+  BackwardRight(150);
+  delay(500);                           //moves againt opposit wall with back
+  stop();
+
+  SENSORS_CALIBRATED = true;
+
+  display_print("Now facing Ballgrabber", 1);
+    digitalWrite(LED_GREEN, HIGH);
+    start();
+
+    for(int i=0; i<measurements_maze; i++){
+        Distanz_Messung_Sensoren();
+        for(int j=0; j<7; j++) {
+            if (j == 4 || j == 5) continue;
+            ballgrabber_values[j] += Distance_Sensor[j];
+        }
+    }
+
+    display_print("Now facing away from Ballgrabber", 1);
+    digitalWrite(LED_GREEN, HIGH);
+    start();
+    for(int i=0; i<measurements_maze; i++){
+        Distanz_Messung_Sensoren();
+        for(int j=0; j<7; j++) {
+            if (j != 4 || j != 5) continue;
+            ballgrabber_values[j] += Distance_Sensor[j];
+        }
+    }
+  calibration_average(measurements_air, measurements_maze);
+}
+
+void calibration_average(int measurements_air, int measurements_maze){
+  for(int i=0 ; i<2 ; i++){     //Left Sensors
+        calibration_sensor[i] = static_cast<int>(std::round(static_cast<double>(air_values[i]) / measurements_air));
+        NeutralSensorValues[i] = static_cast<int>(std::round(static_cast<double>(maze_values[i]) / measurements_maze));
+        ballgrabber_calibration[i] = static_cast<int>(std::round(static_cast<double>(ballgrabber_values[i]) / measurements_maze));
+  }
+  
+  calibration_sensor[2] = static_cast<int>(std::round(static_cast<double>(air_values[2]) /measurements_air));
+  NeutralSensorValues[2] = static_cast<int>(std::round(static_cast<double>(maze_values[2]) / (measurements_maze*2)));     //dopple wall measurment, average PID errors and Offset
+  ballgrabber_calibration[2] = static_cast<int>(std::round(static_cast<double>(ballgrabber_values[2]) / measurements_maze));
+
+
+  for(int i=3 ; i<5 ; i++){     //Right sensors
+        calibration_sensor[i] = static_cast<int>(std::round(static_cast<double>(air_values[i]) / measurements_air));
+        NeutralSensorValues[i] = static_cast<int>(std::round(static_cast<double>(maze_values[i]) / measurements_maze));
+        ballgrabber_calibration[i] = static_cast<int>(std::round(static_cast<double>(ballgrabber_values[i]) / measurements_maze));
+  }
+
+  for(int i=5 ; i<7 ; i++){     //Right sensors
+        calibration_sensor[i] = static_cast<int>(std::round(static_cast<double>(air_values[i]) / measurements_air));
+        NeutralSensorValues[i] = static_cast<int>(std::round(static_cast<double>(maze_values[i]) / (measurements_maze * 2)));
+        ballgrabber_calibration[i] = static_cast<int>(std::round(static_cast<double>(ballgrabber_values[i]) / measurements_maze));
+  }
+
+}
