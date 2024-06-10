7ecb6a9304237ddf9cbc97dc798c1644351ecad3
Daniel M <156967381+Daschka22@users.noreply.github.com>
2024-06-10 20:02:48 +0200

    Gas gas gas added

--- src/Music.h
+++ src/Music.h
@@ -2,26 +2,49 @@
 #include "Setup\Setup.h"
 
 
-//frequencies
-int g7 = 3135;
-int f7 = 2793;
-int e = 2637;
-int dis = 2489;
-int d = 2349;
-int cis = 2217;
-int c = 2093;
-int h = 1975;
-int a = 1760;
-int g = 1567;
-int f = 1396;
-int e6 = 1318;
-int a5 = 880;
-int vp = 0;
+enum Frequencies {
+    b7 = 3729,
+    a7 = 3520,
+    as7 = 3322,
+    g7 = 3135,
+    ges7 = 2959,
+    f7 = 2793,
+    e7 = 2637,
+    dis7 = 2489,
+    d7 = 2349,
+    cis7 = 2217,
+    c7 = 2093,
+    h6 = 1975,
+    b6 = 1864,
+    a6 = 1760,
+    as6 = 1661,
+    g6 = 1567,
+    ges6 = 1479,
+    f6 = 1396,
+    e6 = 1318,
+    es6 = 1244,
+    d6 = 1174,
+    des6 = 1108,
+    c6 = 1046,
+    h5 = 987,
+    b5 = 932,
+    a5 = 880,
+    as5 = 830,
+    g5 = 783,
+    ges5 = 739,
+    f5 = 698,
+    e5 = 659,
+    es5 = 622,
+    c5 = 523,
+    as4 = 415,
+    pa = 0
+};
 
 //length
 double H = 750/1.5;     //halbe
-double P = 562/1.5;     //punktierte
+double PV = 562/1.5;    //punktierte viertel
 double V = 375/1.5;     //viertel 
+double PA = 281/1.5;    //punktierte achtel
 double A = 187/1.5;     //achtel
 double S = 93/1.5;      //sechzehntel
 
@@ -29,31 +52,14 @@ double S = 93/1.5;      //sechzehntel
 double U = 360/1.5;
 double Z = 270/1.5;
 double L = 180/1.5;
+double X = 135/1.5;
 double M = 90/1.5;
 double K = 45/1.5;
 
+//Running in the 90s------------------------------------------------
 
-
-
-
-
-void Imperial_March(){
-    std::vector<int> pitches {880, 880, 880, 685, 1047, 880, 685, 1047, 880};
-    std::vector<int> delayVec {500,500,500,500,250,500,250,500};
-    for(int i = 0; i < pitches.size(); i++){
-        Buzzer_beep(pitches[i], 1);
-        delay(delayVec[i]);
-    }
-}
-
-
-
-//Running in the 90s---------------------------------------------------------------
-
-
-//different motives
 void beat0 (){
-int beat0 []      {e, dis, a5, e ,dis ,a5};
+int beat0 []      {e7, dis7, a5, e7 ,dis7 ,a5};
 double length0 [] {A, A,   V,  A, A,   V};
 double pause0 []  {M, M,   L,  M, M,   L};
 
@@ -64,7 +70,7 @@ for(int i = 0; i < sizeof(beat0) / sizeof(beat0[0]); i++){
 }
 
 void beat1(){
-int beat1 []      {e, dis, d, cis, c, cis, d, dis};
+int beat1 []      {e7, dis7, d7, cis7, c7, cis7, d7, dis7};
 double length1 [] {A, A,   A, A,   A, A,   A, A};
 double pause1 []  {M, M,   M, M,   M, M,   M, M};
 
@@ -75,7 +81,7 @@ for(int i = 0; i < sizeof(beat1) / sizeof(beat1[0]); i++){
 }
 
 void beat2(){
-int beat2 []      {a, a, e, d, c, g, a, a, e, d, c, g};
+int beat2 []      {a6, a6, e7, d7, c7, g6, a6, a6, e7, d7, c7, g6};
 double length2 [] {A, S, S, S, S, A, A, S, S, S, S, A};
 double pause2 []  {M, K, K, K, K ,M, M, K, K, K, K, M};
 
@@ -86,7 +92,7 @@ for(int i = 0; i < sizeof(beat2) / sizeof(beat2[0]); i++){
 }
 
 void beat3(){
-int beat3 []      {c, h, g, c, h, g, c, h, g, d, h, g};
+int beat3 []      {c7, h6, g6, c7, h6, g6, c7, h6, g6, d7, h6, g6};
 double length3 [] {S, S, S, A, S, A, S, S, S, A, S, A};
 double pause3 []  {K, K, K, M, K, M, K, K, K, M, K, M};
 
@@ -97,7 +103,7 @@ for(int i = 0; i < sizeof(beat3) / sizeof(beat3[0]); i++){
 }
 
 void beat4(){
-int beat4 []      {c, c, h};
+int beat4 []      {c7, c7, h6};
 double length4 [] {H, V, V};
 double pause4 []  {U, L, L};
 
@@ -108,7 +114,7 @@ for(int i = 0; i < sizeof(beat4) / sizeof(beat4[0]); i++){
 }
 
 void beat5(){
-int beat5 []      {a, e6, a, e, d, c, g, a, e6, a, e, d, c, g};
+int beat5 []      {a6, e6, a6, e7, d7, c7, g6, a6, e6, a6, e7, d7, c7, g6};
 double length5 [] {S, S,  S, S, S, S, A, S, S,  S, S, S, S, A};
 double pause5 []  {K, K,  K, K, K, K, M, K, K,  K, K, K, K, M};
 
@@ -119,7 +125,7 @@ for(int i = 0; i < sizeof(beat5) / sizeof(beat5[0]); i++){
 }
 
 void beat6(){
-int beat6 []      {c, c, c, d, d, e, e, e, d, d};
+int beat6 []      {c7, c7, c7, d7, d7, e7, e7, e7, d7, d7};
 double length6 [] {S, A, S, A, A, S, A, S, A, A};
 double pause6 []  {K, M, K, M, M, K, M, K, M, M};
 
@@ -130,7 +136,7 @@ for(int i = 0; i < sizeof(beat6) / sizeof(beat6[0]); i++){
 }
 
 void beat7(){
-int beat7 []      {a, a, g, a, a, a, h, c, d};
+int beat7 []      {a6, a6, g6, a6, a6, a6, h6, c7, d7};
 double length7 [] {V, V, A, A, V, V, V, V, V};
 double pause7 []  {L, L, M, M, L, L, L, L, L};
 
@@ -141,7 +147,7 @@ for(int i = 0; i < sizeof(beat7) / sizeof(beat7[0]); i++){
 }
 
 void beat8(){
-int beat8 []      {e, e, d, d, d, c};
+int beat8 []      {e7, e7, d7, d7, d7, c7};
 double length8 [] {V, V, A, A, V, V, V, V, V};
 double pause8 []  {L, L, M, M, L, L, L, L, L};
 
@@ -152,7 +158,7 @@ for(int i = 0; i < sizeof(beat8) / sizeof(beat8[0]); i++){
 }
 
 void beat9(){
-int beat9 []      {e, e, d, d, d, c};
+int beat9 []      {e7, e7, d7, d7, d7, c7};
 double length9 [] {V, V, V, A, V, V};
 double pause9 []  {L, L, L, M, L, L};
 
@@ -163,8 +169,8 @@ for(int i = 0; i < sizeof(beat9) / sizeof(beat9[0]); i++){
 }
 
 void beat10(){
-int beat10 []      {a, c, d, d, c, d, g7, e};
-double length10 [] {A, A, V, A, A, V, A, P};
+int beat10 []      {a6, c7, d7, d7, c7, d7, g7, e7};
+double length10 [] {A, A, V, A, A, V, A, PV};
 double pause10 []  {M, M, L, M, M, L, M, Z};
 
 for(int i = 0; i < sizeof(beat10) / sizeof(beat10[0]); i++){
@@ -174,7 +180,7 @@ for(int i = 0; i < sizeof(beat10) / sizeof(beat10[0]); i++){
 }
 
 void beat11(){
-int beat11 []      {g7, e};
+int beat11 []      {g7, e7};
 double length11 [] {V,  V};
 double pause11 []  {L,  L};
 
@@ -185,8 +191,8 @@ for(int i = 0; i < sizeof(beat11) / sizeof(beat11[0]); i++){
 }
 
 void beat12(){
-int beat12 []      {d, c, d, c, d, c, e};
-double length12 [] {V, A, A, A, V, A, P};
+int beat12 []      {d7, c7, d7, c7, d7, c7, e7};
+double length12 [] {V, A, A, A, V, A, PV};
 double pause12 []  {L, M, M, M, L, M, Z};
 
 for(int i = 0; i < sizeof(beat12) / sizeof(beat12[0]); i++){
@@ -196,7 +202,7 @@ for(int i = 0; i < sizeof(beat12) / sizeof(beat12[0]); i++){
 }
 
 void beat13(){
-int beat13 []      {d, c, d, c, d};
+int beat13 []      {d7, c7, d7, c7, d7};
 double length13 [] {V, A, A, A, V};
 double pause13 []  {L, M, M, M, L};
 
@@ -207,7 +213,7 @@ for(int i = 0; i < sizeof(beat13) / sizeof(beat13[0]); i++){
 }
 
 void beat14(){
-int beat14 []      {e, d, e, d, e};
+int beat14 []      {e7, d7, e7, d7, e7};
 double length14 [] {V, A, A, A, H};
 double pause14 []  {L, M, M, M, U};
 
@@ -267,7 +273,166 @@ void Running_in_the_90s (){
 }
 
 
+//GAS GAS GAS--------------------------------------------------------------------
+void G_beat0(){
+        int G_beat0 []      {es6, b5, ges6, es6, des6, pa, es6 ,c6, as4, c6, as5, as4, c6, des6, as5, h5};
+        double G_length0 [] {S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S};
+        double G_pause0 []  {K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K};
 
+        for(int i = 0; i < sizeof(G_beat0) / sizeof(G_beat0[0]); i++){
+        Buzzer_beep(G_beat0[i], 1, G_length0[i]);
+        delay(G_pause0[i]);
+        }
+}
+
+void G_beat1(){
+        int G_beat1 []      {es5, ges5, h5, es5, ges5, h5, es5, ges5, des6, f5, as5, des6, ges5, b5, ges6, f6};
+        double G_length1 [] {S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S};
+        double G_pause1 []  {K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K};
+
+         for(int i = 0; i < sizeof(G_beat1) / sizeof(G_beat1[0]); i++){
+        Buzzer_beep(G_beat1[i], 1, G_length1[i]);
+        delay(G_pause1[i]);
+        }
+}
+void G_beat2(){
+        int G_beat2 []      {es5, ges5, h5, es5, ges5, h5, es5, ges5, des6, f5, as5, des6, pa, as5, b5, des6};
+        double G_length2 [] {S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S, S};
+        double G_pause2 []  {K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K, K};
+
+         for(int i = 0; i < sizeof(G_beat2) / sizeof(G_beat2[0]); i++){
+        Buzzer_beep(G_beat2[i], 1, G_length2[i]);
+        delay(G_pause2[i]);
+        }
+}
+void G_beat3(){
+        int G_beat3 []      {es6, h5, ges6, es6, h5, pa, es6, ges6, es6, h5, c5, b5, as5, b5, des6};
+        double G_length3 [] {S, S, S, S, S, S, S, S, S, S, A, S, S, S, S};
+        double G_pause3 []  {K, K, K, K, K, K, K, K, K, K, M, K, K, K, K};
+
+         for(int i = 0; i < sizeof(G_beat3) / sizeof(G_beat3[0]); i++){
+        Buzzer_beep(G_beat3[i], 1, G_length3[i]);
+        delay(G_pause3[i]);
+        }
+}
+void G_beat4(){
+        int G_beat4 []      {es6, dis7, es6, d6, d7, d6};
+        double G_length4 [] {PA, PA, A, PA, PA, A};
+        double G_pause4 []  {X, X, M, X, X, M};
+
+         for(int i = 0; i < sizeof(G_beat4) / sizeof(G_beat4[0]); i++){
+        Buzzer_beep(G_beat4[i], 1, G_length4[i]);
+        delay(G_pause4[i]);
+        }
+}
+void G_beat5(){
+        int G_beat5 []      {ges6, ges6, ges6, es6, es6, b5, des6, es6, ges6, es6, es6, es6, es6, ges6, es6, b6, as6};
+        double G_length5 [] {V, V, A, A, A, A, A, A, A, H, V, A, A, V, V, H, H};
+        double G_pause5 []  {L, L, M, M, M, M, M, M, M, U, L, M, M, L, L, U, U};
+
+         for(int i = 0; i < sizeof(G_beat5) / sizeof(G_beat5[0]); i++){
+        Buzzer_beep(G_beat5[i], 1, G_length5[i]);
+        delay(G_pause5[i]);
+        }
+}
+void G_beat6(){
+        int G_beat6 []      {ges7, ges7, ges7, dis7, es6, b5, des6, es6, ges6, es6, as6, as6, as6, ges6, ges6, es6, es6, ges7, ges7, ges7, as7, as7, as7, pa};
+        double G_length6 [] {V, V, A, A, A, A, A, A, A, H, V, A, A, A, A, A, V, A, A, V, A, A, V, V};
+        double G_pause6 []  {L, L, M, M, M, M, M, M, M, U, L, M, M, M, M, M, L, M, M, L, M, M, L, L};
+
+         for(int i = 0; i < sizeof(G_beat6) / sizeof(G_beat6[0]); i++){
+        Buzzer_beep(G_beat6[i], 1, G_length6[i]);
+        delay(G_pause6[i]);
+        }
+}
+void G_beat7(){
+        int G_beat7 []      {es6, es6, ges6, as6, b6, es6, es6, ges6, as6, pa, dis7, dis7, ges7, as7, pa, b7, as7, ges7, as7, b7};
+        double G_length7 [] {A, A, A, V, PV, A, A, A, A, H, A, A, A, A, A, V, A, A, V, PV};
+        double G_pause7 []  {M, M, M, L, Z,  M, M, M, M, U, M, M, M, M, M, L, M, M, L, Z};
+
+         for(int i = 0; i < sizeof(G_beat7) / sizeof(G_beat7[0]); i++){
+        Buzzer_beep(G_beat7[i], 1, G_length7[i]);
+        delay(G_pause7[i]);
+        }
+}
+
+void G_beat8(){
+        int G_beat8 []      {as7, b7, as7, ges7, as7, b7, as7, ges7, pa, cis7, h6, ges6, ges6, es6, es6, es6, ges6, pa, as6, pa, b6, f6, f6, ges6, as6};
+        double G_length8 [] {A, A, A, A, A, A, A, A, A, A, A, V, PV, A, A, A, A, A, A, A, V, A, A, V, PV};
+        double G_pause8 []  {M, M, M, M, M, M, M, M, M, M, M, L, Z,  M, M, M, M, M, M, M, L, M, M, L, Z};
+
+         for(int i = 0; i < sizeof(G_beat8) / sizeof(G_beat8[0]); i++){
+        Buzzer_beep(G_beat8[i], 1, G_length8[i]);
+        delay(G_pause8[i]);
+        }
+}
+
+void G_beat9(){
+        int G_beat9 []      {es6, ges6, pa, b6, es6, es6, es6, ges6, es6, des6, es6, cis7, c7, c7, h6, es6, es6, es6, ges6, as6};
+        double G_length9 [] {PV, PV, S, V, A, A, A, A, A, A, PV, A, PV, A, V, A, A, A, A, PV};
+        double G_pause9 []  {Z,  Z,  K, L, M, M, M, M, M, M, Z,  M, Z,  M, L, M, M, M, M, Z};
+
+         for(int i = 0; i < sizeof(G_beat9) / sizeof(G_beat9[0]); i++){
+        Buzzer_beep(G_beat9[i], 1, G_length9[i]);
+        delay(G_pause9[i]);
+        }
+}
+void G_beat10(){
+        int G_beat10 []      {es6, ges6, pa, b6, es6, es6, es6, ges6, as6, ges6, b6, b6, b6, as6, ges6, as6, b6};
+        double G_length10 [] {PV, PV, S, V, A, A, A, A, A, A, V, A, A, V, V, PV, H};
+        double G_pause10 []  {Z,  Z,  K, L, M, M, M, M, M, M, L, M, M, L, L, Z,  U};
+
+         for(int i = 0; i < sizeof(G_beat10) / sizeof(G_beat10[0]); i++){
+        Buzzer_beep(G_beat10[i], 1, G_length10[i]);
+        delay(G_pause10[i]);
+        }
+}
+void G_beat11(){
+        int G_beat11 [] {};
+        double G_length11 [] {};
+        double G_pause11 [] {};
+
+         for(int i = 0; i < sizeof(G_beat11) / sizeof(G_beat11[0]); i++){
+        Buzzer_beep(G_beat11[i], 1, G_length11[i]);
+        delay(G_pause11[i]);
+        }
+}
+
+void Gas_song(){
+        G_beat0();
+        G_beat1();
+        G_beat0();
+        G_beat2();
+        G_beat0();
+        G_beat1();
+        G_beat3();
+        G_beat4();
+        G_beat5();
+        G_beat6();
+        G_beat7();
+        G_beat8();
+        G_beat9();
+        G_beat10();
+        G_beat9();
+        G_beat10();
+        G_beat0();
+        G_beat1();
+        G_beat0();
+        G_beat2();
+        G_beat0();
+        G_beat1();
+        G_beat3();
+}
+
+//Imperial March-----------------------------------------------------------------
+void Imperial_March(){
+    std::vector<int> pitches {880, 880, 880, 685, 1047, 880, 685, 1047, 880};
+    std::vector<int> delayVec {500,500,500,500,250,500,250,500};
+    for(int i = 0; i < pitches.size(); i++){
+        Buzzer_beep(pitches[i], 1);
+        delay(delayVec[i]);
+    }
+}
 
 
     
